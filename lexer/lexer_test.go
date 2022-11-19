0552a4650200d6c67bf5b09c0f7772a751a153ef
Faustin Date <date.faustin@moneyforward.co.jp>
2022-11-19 10:09:56 +0900

    token and lexer

---
+++ lexer/lexer_test.go
@@ -0,0 +1,43 @@
+package lexer_test
+
+import (
+	"testing"
+
+	"github.com/faustind/monkey/lexer"
+	"github.com/faustind/monkey/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := "=+(){},;"
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+	}
+
+	l := lexer.New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("test[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("test[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+
+	}
+}
